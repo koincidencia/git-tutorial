87e4b29dc5306df792cb68329ced82deb3f4cfb5
leventekobor94 <levente.kobor94@gmail.com>
2017-07-28 09:39:20 +0200

    add a cool func

--- main.c
+++ main.c
@@ -6,9 +6,18 @@ void my_1st_print_func(){
     printf("\n\n\tthis is my func!\t\n\n");
 }
 
+int this_is_the_coolest_func(){
+
+    int a = 2;
+    int b = 7;
+    int c = a + b;
+    return c;
+}
+
 int main()
 {
     my_1st_print_func();
+    printf("%d\n", this_is_the_coolest_func());
     printf("Hello world!\n");
     return 0;
 }
