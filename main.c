ce8f3c2ce9adf67b5ac4b2881b3310f0742c06ae
koincidencia <kazykazy91@gmail.com>
2017-07-28 09:37:09 +0200

    add kazyfunc

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void kazyfunc()
+{
+    printf("en vagyok a kazy\n");
+}
+
+
+
 int main()
 {
     printf("Hello world!\n");
+
+    kazyfunc();
+
     return 0;
 }
