0ef4e3137ec36e4ff88ae53dcf94401c25d8f308
leventekobor94 <levente.kobor94@gmail.com>
2017-07-28 09:37:07 +0200

    add my func

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void my_1st_print_func(){
+
+    printf("\n\n\tthis is my func!\t\n\n");
+}
+
 int main()
 {
+    my_1st_print_func();
     printf("Hello world!\n");
     return 0;
 }
